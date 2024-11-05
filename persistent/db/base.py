45946bb6b6173de7115e985e4fef1b89f6e5315d
Maxim <kuznetsov2acc@gmail.com>
2024-11-05 20:23:38 +0300

    5.11/20:23

--- persistent/db/base.py
+++ persistent/db/base.py
@@ -2,7 +2,7 @@ from sqlalchemy import MetaData
 from sqlalchemy.orm import declarative_base
 import uuid
 
-Base = declarative_base
+Base = declarative_base()
 
 def uuid4_as_str() -> str:
     return (str(uuid.uuid4))
