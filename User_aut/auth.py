45438d9bf1e03fb00acff6951c47f5157149887b
Maxim <kuznetsov2acc@gmail.com>
2024-11-14 17:52:46 +0300

    comment 1

--- User_aut/auth.py
+++ User_aut/auth.py
@@ -23,3 +23,4 @@ def create_access_token(data: dict) -> str:
     auth_data = get_auth_data()
     encode_jwt = jwt.encode(to_encode, auth_data['secret_key'], algorithm=auth_data['algorithm'])
     return encode_jwt
+#fuck me
