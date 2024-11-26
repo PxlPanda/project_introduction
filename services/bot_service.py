abd3f82bea12623031f992710f0ba6c0ad7cf451
Maxim <kuznetsov2acc@gmail.com>
2024-11-26 11:32:56 +0300

    TG-BOT CREATION

---
+++ services/bot_service.py
@@ -0,0 +1,25 @@
+import asyncio
+import logging
+
+import os
+from dotenv import load_dotenv#type:ignore
+
+from aiogram import Bot, Dispatcher
+from aiogram.enums.parse_mode import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+
+import config#type:ignore
+from tg_bot.handlers import router
+
+load_dotenv()
+
+async def main():
+    bot = Bot(token = os.getenv("TG_BOT_TOKEN"))
+    dp = Dispatcher(storage = MemoryStorage())
+    dp.include_router(router)
+    async with asyncio.TaskGroup() as tg:
+        tg.create_task(bot.delete_webhook(drop_pending_updates=True))
+        tg.create_task(dp.start_polling(bot, allowed_updates = dp.resolve_used_update_types()))
+
+logging.basicConfig(level = logging.INFO)
+asyncio.run(main)
