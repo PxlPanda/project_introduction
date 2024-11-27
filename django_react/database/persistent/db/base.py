cef0a43b5fd2e61f5c4abc1ce7dc95054fe35ef7
PxlPanda <pxlpanda.pxldog@gmail.com>
2024-11-27 07:04:05 +0300

    cleaning

--- django_react/database/persistent/db/base.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import MetaData
-from sqlalchemy.orm import declarative_base
-import uuid
-
-Base = declarative_base()
-
-
-def uuid4_as_str() -> str:
-    return (str(uuid.uuid4()))
